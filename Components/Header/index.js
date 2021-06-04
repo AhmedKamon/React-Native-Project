ea430a937c05890aae9adfea766fcbfedb64871f
AhmedKamon <76879119+AhmedKamon@users.noreply.github.com>
2021-06-04 19:35:53 +0600

    Update index.js

--- Components/Header/index.js
+++ Components/Header/index.js
@@ -1,18 +1,1 @@
-import React from 'react'
-import { View, Image,Text  } from 'react-native';
-import styles from './styles'
 
-
-
-const Header = (props) => {
-
-    return (
-        <View style={styles.container}>
-            {/* <Image style={styles.logo} source={require('../../assets/image/car.png')} /> */}
-            <Text  style={styles.logo}>Car City</Text>
-            <Image style={styles.menu} source={require('../../assets/image/menu.png')} />
-        </View>
-    );
-};
-
-export default Header;
